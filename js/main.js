63a19b57cd8414d7ea91b1d1c7d15a05a0114284
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-08-30 19:40:43 +0200

    add fullscreen option

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+'use strict'
+
 $('#fullscreen').click(function () {
     if (document.fullscreenElement) closeFullscreen();
-    else openFullscreen(document.documentElement);
+    else openFullscreen($('main')[0]);
 });
+
 document.documentElement.onfullscreenchange = function() {
-    if (document.fullscreenElement)
+    if (document.fullscreenElement) {
         $('#fullscreen').text('fullscreen_exit');
-    else
+        $(".game").css('--scale-factor', 1);
+    }
+    else {
         $('#fullscreen').text('fullscreen');
+        $(".game").css('--scale-factor', 0.8);
+    }
 }
